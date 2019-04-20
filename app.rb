067211ae8df2350c975100803547fba73eafba7d
Audra Tallman <audi.tallman@gmail.com>
2019-04-20 10:57:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  
+  get '/name' do
+    "My name is Audra."
+  end 
+  
+  get '/hometown' do
+    "My hometown is Grand Island."
+  end 
+  
+  get '/favorite-song' do
+    "My favorite song is 'Sound of Silence' by Simon & Garfunkel."
+  end   
+  
 end
