d9c1e71e7c82edea47cf77a32e27e2d15d30381e
filmscore88 <cmtmorales24@gmail.com>
2019-04-21 14:21:41 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Main page"
+  end
+  
+  get '/name' do
+    "My name is Alberto Morales"
+  end
+
+  get '/hometown' do
+  "My hometown is Chicago"
+  end
+
+  get '/favorite-song' do
+  "My favorite song is 'Oceans by Hillsong'"
+  end
+
 end
