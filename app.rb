4b61a9f9e673a736d9a061c440d70ab037b247e2
victorious-messaging-4309 <wildayk@gmail.com>
2019-04-22 17:22:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Katie."
+  end
+
+  get '/hometown' do
+    "My hometown is Galway."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Three Cheers for Five Years."
+  end
 end
